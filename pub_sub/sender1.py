2e9f5feac43d96f816063ea19e96a298cf36bd62
Howard.Chunag <chuanghao7848@gmail.com>
2019-07-01 14:57:05 +0800

    Add files via upload

---
+++ pub_sub/sender1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+import sys
+import time
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+route_key = 'send1'
+
+strs = ['s', 'm' , 'd', 'f', 'u', 'ah']
+
+for s in strs:
+	channel.basic_publish(exchange='direct_logs', routing_key=route_key, body=s)
+	print(" [x] Sent route_key %s :%s " % (route_key, s))	
+	time.sleep(5)
+
+connection.close()
